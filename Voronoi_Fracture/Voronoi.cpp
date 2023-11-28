135e1653bd5fbf783ac347b79e5cfcf3a28081a0
Thomas-Coulson <A0050289@live.tees.ac.uk>
2023-11-28 16:51:10 +0000

    Added Voxel grid and mapped feature points to it

--- Voronoi_Fracture/Voronoi.cpp
+++ Voronoi_Fracture/Voronoi.cpp
@@ -10,6 +10,21 @@ Voronoi::Voronoi()
         RandomiseFeaturePointPosition(featurepoint);
         m_featurePoints.push_back(featurepoint);
     }
+
+    int voxelWidth = m_cubeSize / m_voxelSubdivisionSize;//200
+    int voxelHeight = m_cubeSize / m_voxelSubdivisionSize;//200
+
+    for (int y = 0; y < voxelHeight; y++)
+    {
+        for (int x = 0; x < voxelWidth; x++)
+        {
+            int voxelX = x * m_voxelSubdivisionSize + (m_voxelSubdivisionSize / 2) + m_borderWidth; // position of midpoint in subdivision
+            int voxelY = y * m_voxelSubdivisionSize + (m_voxelSubdivisionSize / 2) + m_borderWidth; // position of midpoint in subdivision
+            m_voxelSubdivision.push_back(sf::Vector2f(voxelX, voxelY));
+        }
+    }
+    
+    std::cout << "Number of voxels = " << m_voxelSubdivision.size() << std::endl;;
     
 }
 
@@ -33,10 +48,35 @@ void Voronoi::Run()
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
             {
-                for (FeaturePoint point : m_featurePoints)
+                for (auto& point : m_featurePoints)
                 {
                     RandomiseFeaturePointPosition(point);
                 }
+                m_startedSim = false;
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            {
+                m_sampleSize += 1;
+                std::cout << "New Sample Size = " << m_sampleSize << std::endl;
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            {
+                m_sampleSize -= 1;
+                std::cout << "New Sample Size = " << m_sampleSize << std::endl;
+            }
+
+            if (sf::Keyboard::isKeyPressed (sf::Keyboard::Enter))
+            {
+                //start algorithm
+                if (!m_startedSim)
+                {
+                    FindVertecies();
+                    std::cout << "started simulation" << std::endl;
+                    m_startedSim = true;
+                }
+
             }
         }
 
@@ -58,14 +98,48 @@ void Voronoi::Update()
 
 }
 
+void Voronoi::FindVertecies()
+{
+    m_voxelFeaturePoints.clear();
+
+    //find voxels that contain the feature points
+    for (auto& voxelPos : m_voxelSubdivision)
+    {
+        for (auto& featurePoint : m_featurePoints)
+        {
+            if (featurePoint.position.x >= (voxelPos.x - (m_voxelSubdivisionSize / 2)) && featurePoint.position.x < voxelPos.x + (m_voxelSubdivisionSize / 2)
+                && featurePoint.position.y >= voxelPos.y - (m_voxelSubdivisionSize / 2) && featurePoint.position.y < voxelPos.y + (m_voxelSubdivisionSize / 2))
+            {
+                m_voxelFeaturePoints.push_back(voxelPos);
+            }
+        }
+    }
+
+    //find vertecies (points that are equidistant from 3 feature points)
+
+    std::cout << "Number of feature points matched to voxel squares = " << m_voxelFeaturePoints.size() << std::endl;
+
+}
+
 void Voronoi::RandomiseFeaturePointPosition(FeaturePoint& featurePoint)
 {
-    featurePoint.position.x = rand() % 780 + m_borderWidth + 10;
-    featurePoint.position.y = rand() % 780 + m_borderWidth + 10;
+    if (m_sampleSize != 0)
+    {
+        int totalX = 0, totalY = 0;
+        for (int i = 0; i < m_sampleSize; i++)
+        {
+            totalX = totalX + ((rand() % (m_cubeSize - 1)) + m_borderWidth);
+            totalY = totalY + ((rand() % (m_cubeSize - 1)) + m_borderWidth);
+        }
+        featurePoint.position.x = totalX / m_sampleSize;
+        featurePoint.position.y = totalY / m_sampleSize;
+    }
 }
 
 void Voronoi::Draw(sf::RenderWindow& window)
 {
+    
+
     //draw bounding square
     sf::RectangleShape shape(sf::Vector2f(m_cubeSize, m_cubeSize));
     shape.setPosition(sf::Vector2f(m_borderWidth, m_borderWidth));
@@ -74,14 +148,37 @@ void Voronoi::Draw(sf::RenderWindow& window)
     shape.setOutlineThickness(2);
     window.draw(shape);
 
+    DrawVoxels(window);
+
+    DrawFeaturePoints(window);
+}
+
+void Voronoi::DrawFeaturePoints(sf::RenderWindow& window)
+{
     //draw dots
     for (FeaturePoint point : m_featurePoints)
     {
-        sf::CircleShape dot(5);
+        sf::CircleShape dot(2);//radius of 2
+        dot.setOrigin(sf::Vector2f(2, 2));
         dot.setPosition(point.position);
         dot.setFillColor(sf::Color::White);
 
         window.draw(dot);
     }
+}
 
+void Voronoi::DrawVoxels(sf::RenderWindow& window)
+{
+    //draw voxels as squares
+    for (sf::Vector2f boxPos : m_voxelSubdivision)
+    {
+        sf::RectangleShape shape(sf::Vector2f(m_voxelSubdivisionSize, m_voxelSubdivisionSize));
+        shape.setOrigin(sf::Vector2f(m_voxelSubdivisionSize / 2, m_voxelSubdivisionSize / 2));
+        shape.setPosition(boxPos);
+        shape.setFillColor(sf::Color::Black);
+        shape.setOutlineColor(sf::Color(189, 189, 189, 255));
+        shape.setOutlineThickness(1);
+        window.draw(shape);
+    }
+    
 }
--- Voronoi_Fracture/Voronoi.h
+++ Voronoi_Fracture/Voronoi.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include <vector>
 #include <stdlib.h>
 #include <ctime>
@@ -16,15 +17,27 @@ private:
 	sf::Vector2f m_dotPos = sf::Vector2f(500, 500);//first dot position
 	std::vector<FeaturePoint> m_featurePoints;
 
-	int m_totalFeaturePoints = 5;
+	int m_voxelSubdivisionSize = 4; //needs to be a factor of cube size
+	std::vector<sf::Vector2f> m_voxelSubdivision;//stores midpoint positions of subdivisions
+	std::vector<sf::Vector2f> m_voxelFeaturePoints;//stores midpoint positions of subdivisions
+
+	int m_totalFeaturePoints = 20;
 	int m_borderWidth = 100;
 	int m_cubeSize = 800;
+	int m_sampleSize = 10;
+
+	bool m_startedSim = false;
 
 public:
 	Voronoi();
 	void Run();
 	void Update();
+
+	void FindVertecies();
 	void RandomiseFeaturePointPosition(FeaturePoint& featurePoint);
+
 	void Draw(sf::RenderWindow& window);
+	void DrawFeaturePoints(sf::RenderWindow& window);
+	void DrawVoxels(sf::RenderWindow& window);
 };
 
