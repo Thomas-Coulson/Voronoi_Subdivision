ffd8dd54b85033b144f240ac2716bb7127b11292
Thomas-Coulson <A0050289@live.tees.ac.uk>
2023-11-29 03:27:53 +0000

    Improved vertex finding algorithm

--- Voronoi_Fracture/Voronoi.cpp
+++ Voronoi_Fracture/Voronoi.cpp
@@ -2,6 +2,8 @@
 
 Voronoi::Voronoi()
 {
+    srand(time(NULL));
+
     m_window = new sf::RenderWindow(sf::VideoMode(1000, 1000), "Voronoi Subdivision");
 
     for (int i = 0; i < m_totalFeaturePoints; i++)
@@ -30,8 +32,7 @@ Voronoi::Voronoi()
 
 void Voronoi::Run()
 {
-    srand(time(NULL));
-
+    //update loop
     while (m_window->isOpen()) {
 
         sf::Event event;
@@ -48,11 +49,13 @@ void Voronoi::Run()
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
             {
+                Reset();
+                m_startedSim = false;
+
                 for (auto& point : m_featurePoints)
                 {
                     RandomiseFeaturePointPosition(point);
                 }
-                m_startedSim = false;
             }
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
@@ -98,14 +101,22 @@ void Voronoi::Update()
 
 }
 
+void Voronoi::Reset()
+{
+    m_verticies.clear();
+    m_edgePoints.clear();
+    m_voxelFeaturePoints.clear();
+}
+
 void Voronoi::FindVertecies()
 {
-    m_voxelFeaturePoints.clear();
+    std::cout << "Finding Verticies" << std::endl;
+    //m_voxelFeaturePoints.clear();
 
-    //find voxels that contain the feature points
-    for (auto& voxelPos : m_voxelSubdivision)
+    //find voxels that contain the feature points (Dont need this)
+    for (sf::Vector2f voxelPos : m_voxelSubdivision)
     {
-        for (auto& featurePoint : m_featurePoints)
+        for (FeaturePoint featurePoint : m_featurePoints)
         {
             if (featurePoint.position.x >= (voxelPos.x - (m_voxelSubdivisionSize / 2)) && featurePoint.position.x < voxelPos.x + (m_voxelSubdivisionSize / 2)
                 && featurePoint.position.y >= voxelPos.y - (m_voxelSubdivisionSize / 2) && featurePoint.position.y < voxelPos.y + (m_voxelSubdivisionSize / 2))
@@ -115,10 +126,94 @@ void Voronoi::FindVertecies()
         }
     }
 
-    //find vertecies (points that are equidistant from 3 feature points)
+    std::cout << "Found Voxel Feature Points" << std::endl;
 
-    std::cout << "Number of feature points matched to voxel squares = " << m_voxelFeaturePoints.size() << std::endl;
+    //Search through voxels
+    //vertex is where each of ITS NEAREST feature points are equi-distant
 
+    std::vector<int> pointDistances;
+    sf::Vector2f point1, point2, point3, midpoint;
+    int dist1, dist2, dist3, currentDist;
+    int xMid, yMid;
+
+    //for each voxel, we find its closet 3 feature points
+    for (sf::Vector2f voxelPoint : m_voxelSubdivision)
+    {
+        pointDistances.clear();
+        for (sf::Vector2f voxFeaturePointDists : m_voxelFeaturePoints)
+        {
+            pointDistances.push_back(DistanceBetweenPoints(voxelPoint, voxFeaturePointDists));
+        }
+
+        std::sort(pointDistances.begin(), pointDistances.end());//sorts all feature point positions in ascending order
+
+        for (sf::Vector2f voxFeaturePoint : m_voxelFeaturePoints)//points 1 2 and 3 are this voxel's 3 closest feature points
+        {
+            currentDist = DistanceBetweenPoints(voxelPoint, voxFeaturePoint);
+            if (currentDist == pointDistances[0])
+                point1 = voxFeaturePoint;
+            else if (currentDist == pointDistances[1])
+                point2 = voxFeaturePoint;
+            else if (currentDist == pointDistances[2])
+                point3 = voxFeaturePoint;
+        }
+
+        //find midpoint of trio
+        xMid = (point1.x + point2.x + point3.x) / 3;// average x positions
+        yMid = (point1.y + point2.y + point3.y) / 3;// average y positions
+        midpoint = sf::Vector2f(xMid, yMid);
+
+        //find midpoint in voxel space
+        for (sf::Vector2f voxelPos : m_voxelSubdivision)
+        {
+            if (midpoint.x >= (voxelPos.x - (m_voxelSubdivisionSize / 2)) && midpoint.x < voxelPos.x + (m_voxelSubdivisionSize / 2)
+                && midpoint.y >= voxelPos.y - (m_voxelSubdivisionSize / 2) && midpoint.y < voxelPos.y + (m_voxelSubdivisionSize / 2))
+            {
+                midpoint = voxelPos;
+            }
+        }
+
+        if (midpoint == voxelPoint)
+        {
+            m_verticies.push_back(voxelPoint);
+        }
+
+        ///////////////////////////////////////////////////////////////////- Testing edge finding
+        //find midpoint of trio
+        xMid = (point1.x + point2.x) / 2;// average x positions
+        yMid = (point1.y + point2.y) / 2;// average y positions
+        midpoint = sf::Vector2f(xMid, yMid);
+
+        //find midpoint in voxel space
+        for (sf::Vector2f voxelPos : m_voxelSubdivision)
+        {
+            if (midpoint.x >= (voxelPos.x - (m_voxelSubdivisionSize / 2)) && midpoint.x < voxelPos.x + (m_voxelSubdivisionSize / 2)
+                && midpoint.y >= voxelPos.y - (m_voxelSubdivisionSize / 2) && midpoint.y < voxelPos.y + (m_voxelSubdivisionSize / 2))
+            {
+                midpoint = voxelPos;
+            }
+        }
+
+        if (midpoint == voxelPoint)
+        {
+            m_edgePoints.push_back(voxelPoint);
+        }
+
+    }
+
+
+    //std::cout << "Number of feature points matched to voxel squares = " << m_voxelFeaturePoints.size() << std::endl;
+    std::cout << "Number of verticies found = " << m_verticies.size() << std::endl;
+
+}
+
+float Voronoi::DistanceBetweenPoints(sf::Vector2f point1, sf::Vector2f point2)
+{
+    int xDist = abs(point2.x - point1.x);
+    int yDist = abs(point2.y - point1.y);
+
+    //return sqrt(pow(xDist, 2) + pow(yDist, 2));
+    return pow(xDist, 2) + pow(yDist, 2);//as this is only ever used for comparrisons, we dont nee
 }
 
 void Voronoi::RandomiseFeaturePointPosition(FeaturePoint& featurePoint)
@@ -138,8 +233,6 @@ void Voronoi::RandomiseFeaturePointPosition(FeaturePoint& featurePoint)
 
 void Voronoi::Draw(sf::RenderWindow& window)
 {
-    
-
     //draw bounding square
     sf::RectangleShape shape(sf::Vector2f(m_cubeSize, m_cubeSize));
     shape.setPosition(sf::Vector2f(m_borderWidth, m_borderWidth));
@@ -151,15 +244,18 @@ void Voronoi::Draw(sf::RenderWindow& window)
     DrawVoxels(window);
 
     DrawFeaturePoints(window);
+
+    DrawVerticies(window);
 }
 
 void Voronoi::DrawFeaturePoints(sf::RenderWindow& window)
 {
     //draw dots
+    int radius = 4;
     for (FeaturePoint point : m_featurePoints)
     {
-        sf::CircleShape dot(2);//radius of 2
-        dot.setOrigin(sf::Vector2f(2, 2));
+        sf::CircleShape dot(radius);
+        dot.setOrigin(sf::Vector2f(radius, radius));//set origin to mid point
         dot.setPosition(point.position);
         dot.setFillColor(sf::Color::White);
 
@@ -181,4 +277,30 @@ void Voronoi::DrawVoxels(sf::RenderWindow& window)
         window.draw(shape);
     }
     
+}
+
+void Voronoi::DrawVerticies(sf::RenderWindow& window)
+{
+    //draw vertecies
+    int radius = 4;
+    for (sf::Vector2f vertex : m_verticies)
+    {
+        sf::CircleShape dot(radius);
+        dot.setOrigin(sf::Vector2f(radius, radius));//set origin to mid point
+        dot.setPosition(vertex);
+        dot.setFillColor(sf::Color::Red);
+
+        window.draw(dot);
+    }
+
+    //draw edges (test)
+    for (sf::Vector2f edge : m_edgePoints)
+    {
+        sf::CircleShape dot(radius);
+        dot.setOrigin(sf::Vector2f(radius, radius));//set origin to mid point
+        dot.setPosition(edge);
+        dot.setFillColor(sf::Color::Magenta);
+
+        window.draw(dot);
+    }
 }
--- Voronoi_Fracture/Voronoi.h
+++ Voronoi_Fracture/Voronoi.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <stdlib.h>
 #include <ctime>
+#include <cmath>
 
 struct FeaturePoint
 {
@@ -14,17 +15,19 @@ class Voronoi
 {
 private:
 	sf::RenderWindow* m_window;
-	sf::Vector2f m_dotPos = sf::Vector2f(500, 500);//first dot position
+	//sf::Vector2f m_dotPos = sf::Vector2f(500, 500);//first dot position
 	std::vector<FeaturePoint> m_featurePoints;
 
-	int m_voxelSubdivisionSize = 4; //needs to be a factor of cube size
+	int m_voxelSubdivisionSize = 10; //needs to be a factor of cube size
 	std::vector<sf::Vector2f> m_voxelSubdivision;//stores midpoint positions of subdivisions
 	std::vector<sf::Vector2f> m_voxelFeaturePoints;//stores midpoint positions of subdivisions
+	std::vector<sf::Vector2f> m_verticies;
+	std::vector<sf::Vector2f> m_edgePoints;
 
-	int m_totalFeaturePoints = 20;
+	int m_totalFeaturePoints = 5;
 	int m_borderWidth = 100;
 	int m_cubeSize = 800;
-	int m_sampleSize = 10;
+	int m_sampleSize = 1;
 
 	bool m_startedSim = false;
 
@@ -32,12 +35,15 @@ public:
 	Voronoi();
 	void Run();
 	void Update();
+	void Reset();
 
 	void FindVertecies();
 	void RandomiseFeaturePointPosition(FeaturePoint& featurePoint);
+	float DistanceBetweenPoints(sf::Vector2f point1, sf::Vector2f point2); //might need to return int
 
 	void Draw(sf::RenderWindow& window);
 	void DrawFeaturePoints(sf::RenderWindow& window);
 	void DrawVoxels(sf::RenderWindow& window);
+	void DrawVerticies(sf::RenderWindow& window);
 };
 
